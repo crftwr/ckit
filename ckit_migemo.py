048a90e853b9ebcf720fe937b09d2b9ba744a5dd
crftwr <craftware@gmail.com>
2020-01-02 10:46:05 -0800

    Migemo の 64bit 版対応

--- ckit_migemo.py
+++ ckit_migemo.py
@@ -12,36 +12,38 @@ class Migemo:
 
     def __init__( self, dict_dirname ):
         
-        # FIXME : migemo.dll is 32bit
-
         self.dll = ctypes.WinDLL('migemo.dll')
+        self.dll.migemo_open.restype = ctypes.c_void_p
+        self.dll.migemo_query.restype = ctypes.c_void_p
+
         self.handle = self.dll.migemo_open(0)
-        
+
         self.dictionary_ready = False
         
-        result = self.dll.migemo_load( self.handle, Migemo.DICTID_MIGEMO,    os.path.join(dict_dirname,"migemo-dict").encode("mbcs") )
+        result = self.dll.migemo_load( ctypes.c_void_p(self.handle), Migemo.DICTID_MIGEMO,    os.path.join(dict_dirname,"migemo-dict").encode("mbcs") )
         if not result : return
 
-        result = self.dll.migemo_load( self.handle, Migemo.DICTID_ROMA2HIRA, os.path.join(dict_dirname,"roma2hira.dat").encode("mbcs") )
+        result = self.dll.migemo_load( ctypes.c_void_p(self.handle), Migemo.DICTID_ROMA2HIRA, os.path.join(dict_dirname,"roma2hira.dat").encode("mbcs") )
         if not result : return
 
-        result = self.dll.migemo_load( self.handle, Migemo.DICTID_HIRA2KATA, os.path.join(dict_dirname,"hira2kata.dat").encode("mbcs") )
+        result = self.dll.migemo_load( ctypes.c_void_p(self.handle), Migemo.DICTID_HIRA2KATA, os.path.join(dict_dirname,"hira2kata.dat").encode("mbcs") )
         if not result : return
 
-        result = self.dll.migemo_load( self.handle, Migemo.DICTID_HAN2ZEN,   os.path.join(dict_dirname,"han2zen.dat").encode("mbcs") )
+        result = self.dll.migemo_load( ctypes.c_void_p(self.handle), Migemo.DICTID_HAN2ZEN,   os.path.join(dict_dirname,"han2zen.dat").encode("mbcs") )
         if not result : return
 
-        result = self.dll.migemo_load( self.handle, Migemo.DICTID_ZEN2HAN,   os.path.join(dict_dirname,"zen2han.dat").encode("mbcs") )
+        result = self.dll.migemo_load( ctypes.c_void_p(self.handle), Migemo.DICTID_ZEN2HAN,   os.path.join(dict_dirname,"zen2han.dat").encode("mbcs") )
         if not result : return
-        
+
         self.dictionary_ready = True
     
     def isDictionaryReady(self):
         return self.dictionary_ready
 
     def query( self, q ):
-        p = self.dll.migemo_query( self.handle, q.encode("utf-8") )
+
+        p = self.dll.migemo_query( ctypes.c_void_p(self.handle), q.encode("utf-8") )
         s = ctypes.c_char_p(p).value.decode("utf-8")
-        self.dll.migemo_release( self.handle, p )
+        self.dll.migemo_release( ctypes.c_void_p(self.handle), ctypes.c_void_p(p) )
         return s
 
